b12caefa837bd1b1409ab5c13eda4798f6aed011
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-10-29 13:02:44 +0700

    fix: revisioin of API extract video to ML

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,12 +4,12 @@ dotenv.config();
 // Example URL and config constants
 const MODEL_URL = process.env.MODEL_URL || "http://127.0.0.1:5000";
 
-const URL_EXPORT_VIDEO = `${MODEL_URL}/export-video`;
+const URL_EXTRACT_VIDEO = `${MODEL_URL}/extract-video`;
 const CHECKER_URL = `${MODEL_URL}/check-connection`;
 const CHECK_VIDEO = `${MODEL_URL}/check-video`;
 
 module.exports = {
-  URL_EXPORT_VIDEO,
+  URL_EXTRACT_VIDEO,
   CHECKER_URL,
   CHECK_VIDEO,
 };
