151215baf2e9bbae0b79cb14c4ae145128a5f750
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-11-04 11:10:59 +0700

    feat: API Get All Fovs by examination id

--- src/controllers/Fov.controllers.js
+++ src/controllers/Fov.controllers.js
@@ -8,3 +8,12 @@ exports.postFOVData = async function (req, res) {
     res.status(500).send({ message: error.message });
   }
 };
+
+exports.getAllFOVByExaminationId = async function (req, res) {
+  try {
+    const result = await fovService.getAllFOVByExaminationId(req.params);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
